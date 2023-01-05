51eaf93b65046ab3b3ee37ce02037c5f061bccb1
Attila Szollosi <sza421@gmail.com>
2023-01-05 13:46:17 +0100

    5

--- exercise4.py
+++ exercise4.py
@@ -1,4 +1,4 @@
-#Tökéletes számok keresése
+#Prím számok keresése
 a = int(input("Írj be egy számot: "))
 lista = []
 for k in range(1, a+1):
@@ -6,7 +6,7 @@ for k in range(1, a+1):
     for i in range(1, k):
         if k % i == 0:
             b = b + i
-    if b == k:
-        lista.append(b)
-print(f"1 és {a} között ezeket a tőkéletes számokat találtam:")
+    if b == 1:
+        lista.append(i)
+print(f"1 és {a} között ezeket a prím számokat találtam:")
 print(lista)
