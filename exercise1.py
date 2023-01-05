8ee3d82627c5bc6b61041059742f53a2aab91db3
Attila Szollosi <sza421@gmail.com>
2023-01-05 09:46:04 +0100

    2

--- exercise1.py
+++ exercise1.py
@@ -2,10 +2,13 @@
 a = int(input("Írj be egy pozitív egész számot: "))
 
 #Ebbe a listába kerülnek az osztók
-lista = []
+#Az 1 már benne van, mert minden szám osztható eggyel
+lista = [1]
 
-for i in range(1, a+1):
+for i in range(2, a):
     if a % i == 0 :
         lista.append(i)
+
+lista.append(a)
 
 print(lista)
